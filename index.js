87b64b28d5dca289bbc79044a597f65d9dcde69b
jose Angel Coronado <joseenan2777678@gmail.com>
2022-09-16 13:40:09 -0400

    tercer desafio iniciando

--- index.js
+++ index.js
@@ -28,7 +28,7 @@
 // }else{
 //     alert("NO ESCOGIO OPERACION!!!!")
 // }
-    // -----------------------------------------------------
+    // ----------------------------------------------------- desafio complementario
 // const suma = (a,b) => a + b 
 // const resta = (a,b) => a - b 
 // const iva = x =>  x * 0.21
@@ -49,78 +49,71 @@
 // let neumatico =(contador,r) => contador * r;
 
 
+// ------------------------------------------------------ desafio 1
+// let neumatico1 = 15;
+// let neumatico2 = 20;
+// let neumatico3 = 9;
+// let uno;
+// let dos;
+// let tres;
 
-let neumatico1 = 15;
-let neumatico2 = 20;
-let neumatico3 = 9;
-let uno;
-let dos;
-let tres;
-
-alert ("Usted actuamente esta en la seccion de neumaticos, y tiene para escojer entre 3 tipos de productos con su repectivo precio, la cual debera escojer uno de las tres opciones,para luego decidir la cantidad que decea comprar y al final saldra su compra total ");
-let pront = Number(prompt(`Neumatico (1): ${neumatico1} $.
-Neumatico (2): ${neumatico2} $.
-Neumatico (3): ${neumatico3} $.`))
+// alert ("Usted actuamente esta en la seccion de neumaticos, y tiene para escojer entre 3 tipos de productos con su repectivo precio, la cual debera escojer uno de las tres opciones,para luego decidir la cantidad que decea comprar y al final saldra su compra total ");
+// let pront = Number(prompt(`Neumatico (1): ${neumatico1} $.
+// Neumatico (2): ${neumatico2} $.
+// Neumatico (3): ${neumatico3} $.`))
 
 
-function algoritmo(){
-    if (pront === 1){
-        uno = Number(prompt(`Neumatico(1) ${neumatico1}$. Cuantos desea?`))
-        alert( uno);
-         dos = uno * neumatico1
-        tres = neumatico1
+// function algoritmo(){
+//     if (pront === 1){
+//         uno = Number(prompt(`Neumatico(1) ${neumatico1}$. Cuantos desea?`))
+//         alert( uno);
+//          dos = uno * neumatico1
+//         tres = neumatico1
 
-        return dos
-    }else if (pront === 2){
-        uno =  Number(prompt(`Neumatico(2) ${neumatico2}$. Cuantos desea?`))
-        alert(uno)
-         dos = uno * neumatico2
-        tres = neumatico2
-        return dos
-    }else if (pront === 3){
-        uno =  Number(prompt(`Neumatico(3) ${neumatico3}$. Cuantos desea?`))
-        alert (uno)
-          dos = uno * neumatico3
-        tres = neumatico3
-        return dos
-    }else{
-        alert("No es un numero valido, debe colocar 1, 2 o 3")
-    }
+//         return dos
+//     }else if (pront === 2){
+//         uno =  Number(prompt(`Neumatico(2) ${neumatico2}$. Cuantos desea?`))
+//         alert(uno)
+//          dos = uno * neumatico2
+//         tres = neumatico2
+//         return dos
+//     }else if (pront === 3){
+//         uno =  Number(prompt(`Neumatico(3) ${neumatico3}$. Cuantos desea?`))
+//         alert (uno)
+//           dos = uno * neumatico3
+//         tres = neumatico3
+//         return dos
+//     }else{
+//         alert("No es un numero valido, debe colocar 1, 2 o 3")
+//     }
     
+// }
+// algoritmo();
+
+// alert(`gracias por su compra
+// actualmente lleva
+// ${uno} compras de ${tres}$
+// total: ${uno * tres}$.`);
+
+
+
+
+// ------------------------------------desafio complementario 2
+
+let persona;
+const personas = [];
+let p;
+
+const acumularP = () =>{
+    for (let i = 0; i <= 4; i++){
+        persona = prompt("ingrese su nombre")
+        personas.push(persona) 
+    }
+    for( let j = 0; j < personas.length; j ++){
+        p = personas[j]
+    }
+    return p
 }
-algoritmo();
-
-alert(`gracias por su compra
-actualmente lleva
-${uno} compras de ${tres}$
-total: ${uno * tres}$.`);
-
-
-
-
-// for( let j = 0; j < 15 ; j++){
-//     if (j === 20){
-//         break
-//     }
-// r = j
-// }
-// const precioProductos = () =>{
-//     for( let i = 1; i < 4 ; i++){
-//         if (i === 5){
-//             break
-//         }
-//         contador = i
-//     }
-// }
-
-
-
-
-
-
-
-// function calcularT(){
-//     return resultado * neumatico(contador,r);
-// }
-// precioProductos();
-// console.log(gastoTotal);
+acumularP();
+// console.log(personas)
+console.log(p)
